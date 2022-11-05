b79e620240614513a6e6eedb6fa93dd633165a81
MatthewVozenilek <mattvoz30@gmail.com>
2022-11-05 15:19:43 -0500

    initial

---
+++ vector.h
@@ -0,0 +1,15 @@
+#ifndef __GL_VECTOR__
+#define __GL_VECOTR__
+namespace vector
+{
+    class vector2 {
+        public:
+        vector2();
+        vector2( int x);
+        vector2(int x, int y);
+        private:
+        
+    };
+}; // namespace name
+
+#endif
