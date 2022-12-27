0bab4442e7c4c9b8e505be6d2794716f23d451f0
unknown <mattvoz30@gmail.com>
2022-12-26 21:17:20 -0600

    add shader class

--- graphicsObjects/shader.cpp
+++ graphicsObjects/shader.cpp
@@ -1,6 +1,6 @@
 #include "shader.h"
-#include <iostream.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cerrno>
 
 shader::shader(){
     vertexShader = NULL;
@@ -8,11 +8,31 @@ shader::shader(){
     shaderProgram = NULL;
 }
 
-shader(std::string vertex, bool vertexFile, std::string fragment, bool fragFile) {
+shader::shader(std::string vertex, bool vertexFile, std::string fragment, bool fragFile) {
+    FILE * fp;
     if( vertexFile ) {
-        std::FILE * fp = std::fopen( vertex, "r");
+        fp = fopen( vertex.c_str(), "r");
         if(fp) {
-            fseek
+            fseek(fp, 0, SEEK_END);
+            vertexShader.resize(ftell(fp));
+            rewind(fp);
+            fread( &vertexShader[0], 1, vertexShader.size(), fp);
+            fclose(fp);
         }
+    } else {
+        vertexShader = vertex.c_str();
+    }
+
+    if(fragFile) {
+        fp = fopen( fragment.c_str(), "r");
+        if(fp) {
+            fseek(fp, 0, SEEK_END);
+            fragmentShader.resize(ftell(fp));
+            rewind(fp);
+            fread( &fragmentShader[0], 1, vertexShader.size(), fp);
+            fclose(fp);
+        }
+    } else {
+        fragmentShader = fragment.c_str();
     }
 }
